e04ef2f42877d194802d66057552142da5216053
Thundasantana <ednilson.sdas@outlook.com>
2024-02-14 22:11:40 -0300

    Created class Movie

---
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -0,0 +1,91 @@
+package com.devsuperior.movieflix.entities;
+
+import java.util.Objects;
+
+public class Movie {
+	
+	private Long id;
+	private String title;
+	private String subTitle;
+	private Integer year;
+	private String imgUri;
+	private String synopsis;
+	
+	
+	public Movie() {
+	}
+
+	public Movie(Long id, String title, String subTitle, Integer year, String imgUri, String synopsis) {
+		this.id = id;
+		this.title = title;
+		this.subTitle = subTitle;
+		this.year = year;
+		this.imgUri = imgUri;
+		this.synopsis = synopsis;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubTitle() {
+		return subTitle;
+	}
+
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getImgUri() {
+		return imgUri;
+	}
+
+	public void setImgUri(String imgUri) {
+		this.imgUri = imgUri;
+	}
+
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		return Objects.equals(id, other.id);
+	}
+}
